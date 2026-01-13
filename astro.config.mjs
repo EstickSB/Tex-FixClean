c7b4133b9c99f0819c8a1f1eb303a06ab8defe4e
EstickSB <esticksilva4@gmail.com>
2026-01-13 00:19:19 -0500

    Configure Astro for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  site: 'https://github.com/EstickSB',
+  base: '/Tex-FixClean',
   integrations: [
     react(),
     tailwind()
