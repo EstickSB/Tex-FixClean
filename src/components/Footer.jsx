b94e559fe28a65970b660c7aadaebdc55d16f765
EstickSB <esticksilva4@gmail.com>
2026-01-13 16:46:24 -0500

    feat: Multilingual and responsive design fixed0

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-8 text-[10px] text-slate-600 uppercase tracking-[0.2em] font-medium text-center">
-      Text-FixEditor — 2026
+      Limpieza y Organización de Texto Online — 2026 — <a className="cursor-pointer" href="https://github.com/EstickSB">Meet the author</a>
     </footer>
   );
 };
