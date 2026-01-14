bf62246dd9371fe98a2f6460ccc0cc20ff2f0cdb
EstickSB <esticksilva4@gmail.com>
2026-01-14 10:02:49 -0500

    hotfix: previous version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="mt-8 text-[10px] text-slate-600 uppercase tracking-[0.2em] font-medium text-center">
-      Limpieza de Texto Online — 2026 — <a className="cursor-pointer" href="https://github.com/EstickSB">Meet the author</a>
+      Limpieza de Texto Online — 2026 — <a className="cursor-pointer" href="https://github.com/EstickSB">Meet the author </a><a href="">— Download extension</a>
     </footer>
   );
 };
