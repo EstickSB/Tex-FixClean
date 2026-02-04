42742e9158f12ff69eeddc444bbb3207a7193fed
EstickSB <esticksilva4@gmail.com>
2026-02-03 23:41:45 -0500

    update try 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="flex flex-col items-center mb-2 sm:mb-4 md:mb-8 text-center">
       <h1 className="text-2xl sm:text-4xl md:text-5xl font-black tracking-tighter text-white drop-shadow-2xl">
-        Tex<span className="text-blue-500">Fix</span>Clean
+        Limpiador de Texto<span className="text-blue-500">Online</span>Clean
       </h1>
       <div className="flex items-center gap-3 mt-2 sm:mt-3 md:mt-4">
         <div className="h-1 w-8 bg-blue-500 rounded-full opacity-50"></div>
